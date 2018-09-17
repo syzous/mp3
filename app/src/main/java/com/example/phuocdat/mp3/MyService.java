648a65e26198fbaee560a07b22346ccc01c7401c
khuonglam <vuongkhuonglam@gmail.com>
2018-09-17 11:51:52 +0700

    change to 2

--- app/src/main/java/com/example/phuocdat/mp3/MyService.java
+++ app/src/main/java/com/example/phuocdat/mp3/MyService.java
@@ -25,7 +25,7 @@ public class MyService extends Service {
     public void setmServiceMediaPlay(MediaPlayer mServiceMediaPlay) {
         this.mServiceMediaPlay = mServiceMediaPlay;
     }
-    //1
+    //2
     public MediaPlayer getmServiceMediaPlay() {
         return mServiceMediaPlay;
     }
